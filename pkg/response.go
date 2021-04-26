40cbbabb92b907062b82535de2de057d3fb7005a
sergey <sergey@spyse.com>
2021-04-26 11:04:35 +0300

    feat: added autonomous system search count method

--- pkg/response.go
+++ pkg/response.go
@@ -14,8 +14,6 @@ type Data struct {
 	Items []interface{} `json:"items"`
 	// The total number of records stored in the database
 	TotalCount *int64 `json:"total_count,omitempty"`
-	// Maximum allowed number of records for viewing
-	MaxViewCount *int `json:"max_view_count,omitempty"`
 	// The offset of rows iterator
 	Offset *int `json:"offset,omitempty"`
 	// Received Rows Limit
