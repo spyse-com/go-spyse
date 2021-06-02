2d42704a21fde830d5eb39a21febea137aefde87
icamys <prisacari.dmitrii@gmail.com>
2021-06-02 13:01:56 +0300

    added search operators constants

--- pkg/request.go
+++ pkg/request.go
@@ -6,6 +6,16 @@ import (
 	"net/http"
 )
 
+const SEARCH_OPERATOR_EQUAL = "eq"
+const SEARCH_OPERATOR_NOT_EQUAL = "not_eq"
+const SEARCH_OPERATOR_CONTAINS = "contains"
+const SEARCH_OPERATOR_STARTS_WITH = "starts"
+const SEARCH_OPERATOR_ENDS_WITH = "ends"
+const SEARCH_OPERATOR_EXISTS = "exists"
+const SEARCH_OPERATOR_NOT_EXISTS = "not_exists"
+const SEARCH_OPERATOR_GREATER_THAN_OR_EQUAL = "gte"
+const SEARCH_OPERATOR_LESS_THAN_OR_EQUAL = "lte"
+
 type SearchParameter struct {
 	Operator string `json:"operator"`
 	Value    string `json:"value"`
